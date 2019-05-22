cf504df1a1fc5f3e5d91685cdde273788de3a02b
LeanderMaatman <124550@student.drenthecollege.nl>
2019-05-22 10:50:30 +0200

    uitgeprinte database
    Co-Authored-By: Falco Hospes <61621@student.drenthecollege.nl>

--- database.php
+++ database.php
@@ -9,5 +9,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT id, voornaam, achternaam, geboortedatum FROM eindopdracht";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Naam: ". $row["voornaam"]. " " . $row["achternaam"] . " - Geboortedatum: ". $row["geboortedatum"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
