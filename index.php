c4064109d4b7ac40e43963ef4ac2edab36f55ce8
LeanderMaatman <124550@student.drenthecollege.nl>
2019-05-28 11:23:12 +0200

    een form toegevoegd nu de form nog een fuctie geven
    Co-Authored-By: Falco Hospes <61621@student.drenthecollege.nl>

--- index.php
+++ index.php
@@ -4,13 +4,32 @@
     <title>database 2</title>
   </head>
   <body>
-<?php 	include "database.php"; ?>
 
 
+<?php
+ include "database.php";
 
-</form>
+$sql = "SELECT id, voornaam, achternaam, geboortedatum FROM eindopdracht";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Naam: ". $row["voornaam"]. " " . $row["achternaam"] . " - Geboortedatum: ". $row["geboortedatum"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
+$conn->close();
+
+?>
+
+<form name="form" method="post" action="database.php" >
+  <select name="id">
+    <option value="1"><?php $row["id"] $row["voornaam"] $row["achternaam"] $row ["geboortedatum"]?></option>
+    <option value="2"><?php $row["id"] $row["voornaam"] $row["achternaam"] $row ["geboortedatum"]?></option>
+    <option value="3"><?php $row["id"] $row["voornaam"] $row["achternaam"] $row ["geboortedatum"]?></option>
+  </select>
 
   </body>
 </html>
